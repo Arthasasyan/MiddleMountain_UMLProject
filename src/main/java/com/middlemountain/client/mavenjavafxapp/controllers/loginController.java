03e50ffccbaec6efc3e5e792721785854c4b9bb8
jnannni <49609088+jnannni@users.noreply.github.com>
2019-04-21 17:01:02 +0300

    Login

--- src/main/java/com/middlemountain/client/mavenjavafxapp/controllers/loginController.java
+++ src/main/java/com/middlemountain/client/mavenjavafxapp/controllers/loginController.java
@@ -1,5 +1,10 @@
 package com.middlemountain.client.mavenjavafxapp.controllers;
 
+import com.middlemountain.client.mavenjavafxapp.MainApp;
+import com.middlemountain.enums.Permission;
+import com.middlemountain.model.Employee;
+import com.middlemountain.service.Service;
+import com.middlemountain.service.TestService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,9 +12,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.stage.Stage;;
 
 public class loginController {
+    private Service service;
 
     @FXML
     private Button enterAuthAction;
@@ -25,16 +31,37 @@ public class loginController {
 
     @FXML
     void initialize() {
+        service = new TestService();
         enterAuthAction.setOnAction(event -> {
             try {
+                Employee employee =  null;
                 Stage stage = new Stage();
                 Stage oldStage = (Stage)enterAuthAction.getScene().getWindow();
-                oldStage.close();
-                Parent root = FXMLLoader.load(getClass().getResource("../fxml/worker.fxml"));
-                stage.setTitle("Окно работника");
-                stage.setResizable(false);
-                stage.setScene(new Scene(root));
-                stage.show();
+                oldStage.hide();
+                MainApp.oldestStage.close();
+                String loginText = loginInputAction.getText().trim();
+                String passwordText = passwordInputAction.getText().trim();
+                if(!loginText.equals("") && !passwordText.equals("")) {
+                    try  {
+                        employee = service.login(loginText, passwordText);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        throw e;
+                    }
+                    if( service.login(loginText, passwordText).getPermission().equals(Permission.EMPLOYEE)) {
+                        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("worker.fxml"));
+                        stage.setTitle("Worker's window");
+                        stage.setResizable(false);
+                        stage.setScene(new Scene(root));
+                        stage.show();
+                    } else if ( service.login(loginText, passwordText).getPermission().equals(Permission.MANAGER)) {
+                        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("manager.fxml"));
+                        stage.setTitle("Manager's window");
+                        stage.setResizable(false);
+                        stage.setScene(new Scene(root));
+                        stage.show();
+                    }
+                } else System.out.println("Error");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -51,3 +78,4 @@ public class loginController {
     }
 
 }
+
--- src/main/java/com/middlemountain/client/mavenjavafxapp/controllers/managerController.java
+++ src/main/java/com/middlemountain/client/mavenjavafxapp/controllers/managerController.java
@@ -3,7 +3,11 @@ package com.middlemountain.client.mavenjavafxapp.controllers;
 import com.middlemountain.model.Good;
 import com.middlemountain.service.Service;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class managerController {
 
@@ -30,10 +34,16 @@ public class managerController {
 
     @FXML
     void initialize(Good good) {
-        deleteGoodAction.setOnAction(event -> {
-            Service service = null;
+        exitButtonAction.setOnAction(event -> {
+            Stage oldStage = (Stage)exitButtonAction.getScene().getWindow();
+            oldStage.close();
+            Stage stage = new Stage();
             try {
-                service.deleteGood(good);
+                Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+                stage.setTitle("Magic Shop");
+                stage.setResizable(false);
+                stage.setScene(new Scene(root));
+                stage.show();
             } catch (Exception e) {
                 e.printStackTrace();
             }
